164ed7c489b53209b72761d79e6eaa5355814fce
十一 <1318720440@qq.com>
2023-10-24 15:25:06 +0800

    fix:

--- src/pages/Project/ProjectLit/components/addTask.js
+++ src/pages/Project/ProjectLit/components/addTask.js
@@ -18,7 +18,7 @@ function AddTask(props) {
     const [addForm, setAddForm] = useState({})
     const getSetForm = (e) => {
         // setAddForm(e)
-        console.log(e, 'e')
+        // console.log(e, 'e')
     }
     const { Option } = Select
     // const { onReset } = props
